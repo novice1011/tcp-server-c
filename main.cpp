570867df7e8393402763566fac97f0c2d2ca1ce8
novice1011 <mqomaruzzaman@gmail.com>
2019-10-26 23:18:54 +0800

    working: read only server with reconnect after client disconnect

--- main.cpp
+++ main.cpp
@@ -13,57 +13,26 @@
 #define PORT 8080
 #define SA struct sockaddr
 
-// Function designed for chat between client and server.
-void func(int sockfd)
-{
-    char buff[MAX];
-    int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, MAX);
-
-        // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, MAX);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n');
-
-        // and send that buffer to client
-        write(sockfd, buff, sizeof(buff));
-
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
-}
-
-// Driver function
-int main()
-{
-    int sockfd, connfd;//, len;
+void reconnect(int *sockfd, int *connfd){
     struct sockaddr_in servaddr, cli;
     socklen_t len = sizeof(cli);
-
     // socket create and verification
     //1.domain 2.type 3. protocol http://man7.org/linux/man-pages/man2/socket.2.html#top_of_page
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
+    close(*sockfd);
+    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int option = 1;
+    setsockopt(*sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+
+    if (*sockfd == -1) {
         printf("socket creation failed...\n");
         exit(0);
     }
     else
         printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr)); //create memory
-//    fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 
     // assign IP, PORT
-    ;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);//192.168.0.104
     servaddr.sin_port = htons(PORT);
     /*
      * INADDR_ANY :
@@ -72,9 +41,8 @@ int main()
      * the syntax would be my_sockaddress.sin_addr.s_addr = inet_addr("127.0.0.1");
      * then call bind(my_socket, (SOCKADDR *) &my_sockaddr, ...).
      */
-
     // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+    if ((bind(*sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
         printf("socket bind failed...\n");
         exit(0);
     }
@@ -84,7 +52,7 @@ int main()
     // Now server is ready to listen and verification
     //arguments: sockfd:file descriptor,backlog:argument defines the maximum length to which the queue of
     //pending connections for sockfd may grow
-    if ((listen(sockfd, 5)) != 0) {
+    if ((listen(*sockfd, 5)) != 0) {
         printf("Listen failed...\n");
         exit(0);
     }
@@ -92,19 +60,54 @@ int main()
         printf("Server listening..\n");
 
     // Accept the data packet from client and verification
-//    connfd = accept(sockfd, (SA*)&cli, &len);
-    // server side - see man page for accept4 under linux
-    connfd = accept4(sockfd, (SA*)&cli, &len, SOCK_NONBLOCK);
-    if (connfd < 0) {
+    *connfd = accept(*sockfd, (SA*)&cli, &len);
+    if (*connfd < 0) {
         printf("server acccept failed...\n");
         exit(0);
     }
     else
         printf("server acccept the client...\n");
+}
 
-    // Function for chatting between client and server
-    func(connfd);
+// Function designed for chat between client and server.
+int func(int connfd)
+{
+    char buff[MAX];
+    ssize_t x = sizeof (long int);
 
-    // After chatting close the socket
-    close(sockfd);
+    // infinite loop for chat
+    for (;;) {
+        bzero(buff, MAX);
+
+        x = read(connfd, buff, sizeof(buff));   // read the message from client and copy it in buffer
+        printf("%s", buff);                     // print buffer which contains the client contents
+
+        if (x<=0){
+            printf(".........connection break :(\n\n");
+            return -1;
+        }
+
+        // if msg contains "Exit" then server exit and chat ended.
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            return 0;
+        }
+    }
+}
+
+// Driver function
+int main()
+{
+    int sockfd = 0, connfd = 0;
+    reconnect(&sockfd,&connfd);
+
+    while (1) {
+        if (func(connfd)<0){
+            close(sockfd);
+            reconnect(&sockfd,&connfd);
+        }
+        else if (func(connfd)==0) {
+            close(sockfd);
+        }
+    }
 }
